0c958f51e2c342026960c66ef1763b120892eff5
vasilev.mihail21 <vasilev.mihail21@wb.ru>
2022-09-17 01:57:48 +0300

    feat: add button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next';
-import { Htag } from '../components';
+import { Button, Htag } from '../components';
 
 const Home: NextPage = () => {
   return (
     <>
       <Htag tag='h1'>Text</Htag>
+      <Button appearance='primary' arrow='right'>Button</Button>
+      <Button appearance='ghost' arrow='down'>Button</Button>
     </>
   );
 };
