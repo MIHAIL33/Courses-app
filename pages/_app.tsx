a06f7aac13e20c3ff23279c9501d63342fb33f2f
vasilev.mihail21 <vasilev.mihail21@wb.ru>
2022-09-16 22:32:50 +0300

    feat: add Htag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>MyTop</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, { Html, Head, NextScript, DocumentContext, DocumentInitialProps, Main } from 'next/document';
 
 class MyDocument extends Document {
 
@@ -10,7 +10,10 @@ class MyDocument extends Document {
     render(): JSX.Element {
         return (
             <Html lang="ru">
-                <Head />
+                <Head>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
