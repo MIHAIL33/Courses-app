f1a1c8298f911b8893d312a0aa5fdf1f2d299ca5
vasilev.mihail21 <vasilev.mihail21@wb.ru>
2022-10-07 01:25:52 +0300

    feat: add hh block

---
+++ page-components/TopPageComponent/TopPage.tsx
@@ -0,0 +1,24 @@
+import { TopPageProps } from "./TopPage.props";
+import { Htag, Tag, HhData } from "../../components";
+import styles from "./TopPage.module.css";
+import { TopLevelCategory } from "../../interfaces/page.interface";
+
+export const TopPageComponent = ({ page, products, firstCategory }: TopPageProps): JSX.Element => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.title}>
+                <Htag tag='h1'>{page.title}</Htag>
+                {products && <Tag color='grey' size='m'>{products.length}</Tag>}
+                <span>Sorting</span>
+            </div>
+            <div>
+                {products && products.map(p => (<div key={p._id}>{p.title}</div>))}
+            </div>
+            <div className={styles.hhTitle}>
+                <Htag tag='h2'>Вакансии - {page.category}</Htag>
+                <Tag color='red' size="m">hh.ru</Tag>
+            </div>
+            {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh}/>}
+        </div>
+    );
+};
